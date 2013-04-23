79637af98a9915a157b96bf49c28aaa9fdc98ccc
j-stark <comj04@gmail.com>
2013-04-23 17:30:54 -0500

    Hola

--- iOS/trunk/web/application/views/pages/index.php
+++ iOS/trunk/web/application/views/pages/index.php
@@ -29,9 +29,27 @@
                     ?>
         <tr>
           <td><a href="<?php base_url(); ?>apps/view/<?php echo $apps[$i]['id']; ?>"><?php echo $apps[$i]['nombre']; ?></a></td>
-          <td><a href="">Eliminar</a></td>
+          <td><a href="#eliminar<?php echo $apps[$i]['id'] ?>">Eliminar</a></td>
           <td><a href="">Exportar</a></td>
         </tr>
+
+
+        <div id="eliminar<?php echo $apps[$i]['id'] ?>" class="modalDialog">
+          <div>
+            <a href="#" title="Close" class="close">X</a>
+            <?php echo validation_errors(); ?>
+            <?php echo form_open('apps/eliminar') ?>
+        
+              <h2><?php echo $apps[$i]['nombre'] ?><br/></h2>
+              <p>Nota: Se eliminaran las recetas relacionadas con esta aplicación.</p>
+        
+              <input type="hidden" name="id" id="id"  value="<?php echo $apps[$i]['id']; ?>"/>
+        
+              <button type="submit" class="eliminarBoton">Eliminar</button>
+            </form>
+          </div>
+        </div>
+
         <?php } } ?>
       </table>
       
