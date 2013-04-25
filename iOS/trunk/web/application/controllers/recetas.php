13d6842f1cea7682d9d4ba4f0dd83d7068a47621
gersa1990 <ger_agui@live.com.mx>
2013-04-25 15:11:41 -0500

    Agregando vistas de recetas

--- iOS/trunk/web/application/controllers/recetas.php
+++ iOS/trunk/web/application/controllers/recetas.php
@@ -10,6 +10,7 @@ class Recetas extends CI_Controller {
 		$this->load->model('categoria_model');
 		$this->load->model('complementarias_model');
 		$this->load->model('video_model');
+		$this->load->library('typography');
 	}
 
 	public function searchByName()
@@ -118,22 +119,141 @@ class Recetas extends CI_Controller {
 		$this->load->view('templates/footer');
 	}
 
-	public function create($app){
+	public function searchById(){
 
-		$this->load->helper('form');
-		$this->load->helper('url');
-		$this->load->library('form_validation');
-		$data['title'] = 'Nueva Receta';
+		$id = $_POST['id_receta'];
+		$id_app = $_POST['id_app'];
 
-		$this->form_validation->set_rules('titulo', 'Título', 'required');
+		$data = $this->recetas_model->searchById($id);
 
-		if ($this->form_validation->run() === FALSE){
-			redirect(base_url()."agregar");
-		}else{
-			$id = $this->recetas_model->set_recetas();
+		echo "<div class='myform'>
 
-			redirect(base_url().'apps/view/'.$app, 'refresh');
+            <h2 class='txcenter'>Nueva receta</h2>
+            <p class='txcenter'>Información de la receta</p>
+            <br><br>";
+
+		echo "<form action='' method='' id='form_recetas2'>";           
+        echo " <label for='titulo' class=fixh1 left'>Título</label>";
+        echo " <input type='text' name='titulo' id='titulo' class='left' value='".$data[0]['titulo']."'/>";
+        echo " <div class='status left error'>Ya existe una receta con este nombre.</div>";
+
+        echo " <div class='clear'></div> <br>";
+        echo " <input type='hidden' name='app' value='".$data[0]['id_app']."''>"; 
+
+        echo " <label for='categoria' class='fixh2'>Categoria</label>";
+        echo " <select name='categoria' id='categoria'>";
+
+        $categorias = $this->App_model->getCategoryFromAppId($id_app);
+
+        foreach ($categorias as $c_item)
+        {
+        	$id 		= $c_item['id'];
+        	$nombre		= $c_item['nombre'];
+
+        	echo "<option value='.$id.'>".$nombre."</option>";
+        }             
+            echo "</select><br>";
+
+            
+            
+        echo "<label for='dificultad'>Dificultad <span class='small'>Dificultad para realizarla</span></label>
+            <select name='dificultad' id='dificultad' class='wt1'>";
+              
+                  for ($i=0; $i < 6; $i++) 
+                  { 
+                  	if($data[0]['dificultad'] == $i)
+                  	{
+                  		echo "<option value='".$i."' selected>".$i."</option>";
+                  	}
+                  	else
+                  	{
+                  		echo "<option value='".$i."'>".$i."</option>";
+                  	}
+                  }
+                  
+        echo "  </select><br><br>";
+            
+            
+            
+        echo "<label for='procedimiento' class='fixmargin'>Procedimiento <span class='small'>Pasos de preparación</span></label>
+            <textarea name='procedimiento' id='procedimiento' title='procedimiento' rows='4' cols='46' required>".$data[0]['procedimiento']."</textarea>
+
+            <br>
+            
+            <label for='ingre' class='fixmargin'>Ingredientes <span class='small'>Lista de ingredientes</span></label>
+            <textarea name='ingredientes' id='ingredientes' title='ingredientes' rows='4' cols='46' required>".$data[0]['ingredientes']."</textarea>
+
+            <br>
+            
+            <label for='prepa'>Preparación <span class='small'>Tiempo en min</span></label>
+            <input type='text' value='".$data[0]['preparacion']."' name='preparacion' id='preparacion' required />
+
+            <br>
+            
+            <label for='coccion'>Cocción <span class='small'>Tiempo en min</span></label>
+            <input type='text' value='".$data[0]['coccion']."' name='coccion' id='coccion' required/>
+
+            <br>
+            
+            <label for='costo'>Costo <span class='small'>Precio aproximado</span></label>
+             <select name='costo' id='costo' class='wt1'>";
+              for ($i=1; $i < 6; $i++) 
+               {
+               		if($data[0]['costo']==$i)
+               		{
+               			echo "<option value=".$i." selected>".$i."</option>";	
+               		}
+               		else
+               		{
+               			echo "<option value=".$i.">".$i."</option>";
+               		}
+               		
+               } 
+            echo "</select>
+            <br><br>
+            
+            <label for='ïmg'>Imagen <span class='small'>Cargar file</span></label>
+            <input type='text' value='".$data[0]['foto']."' name='img' id='foto' required />
+
+            <br><br>
+            
+            <button type='submit' class='button bl2'>Guardar</button>
+
+            </form></div>";
+	}
+
+	public function creates()
+	{
+		$titulo   		= $_POST['titulo'];
+		$id_categoria   = $_POST['id_categoria'];
+		$id_app		    = $_POST['id_app'];
+		$procedimiento  = $_POST['procedimiento'];
+		$ingredientes   = $_POST['ingredientes'];
+		$preparacion    = $_POST['preparacion'];
+		$coccion	    = $_POST['coccion'];
+		$costo 		    = $_POST['costo'];
+		$foto 		    = $_POST['foto'];
+		$user_fav		= $_POST['user_fav'];
+		$dificultad     = $_POST['dificultad'];
+		$preparada		= "0";
+
+		//echo "Titulo: ".$titulo.", Cat: ".$id_categoria.", APP: ".$id_app.", PROC: ".$procedimiento.", ING:  ".$ingredientes.", PREP: ".$preparacion.", coccion: ".$coccion.", costo: ".$costo.", FAV: ".$user_fav.", preparada: ".$preparada;
+		
+
+		$id = $this->recetas_model->set_recetas($titulo, $id_categoria, $id_app, $procedimiento, $ingredientes, $preparacion, $coccion, $costo, $foto, $user_fav, $dificultad, $preparada);
+
+		if($id)
+		{
+			echo $titulo;
 		}
+		else
+		{
+			echo "Ah ocurrido un error inesperado intentalo de nuevo por favor";
+		}
+
+
+		//redirect(base_url().'apps/view/'.$app, 'refresh');
+		
 	}
 
 
