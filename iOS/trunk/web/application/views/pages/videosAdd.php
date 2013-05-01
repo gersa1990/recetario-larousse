16e137f31273b0f8c61452c93cf1ca956a221d87
gersa1990 <ger_agui@live.com.mx>
2013-05-01 10:45:22 -0500

    Agregando cambios

--- iOS/trunk/web/application/views/pages/videosAdd.php
+++ iOS/trunk/web/application/views/pages/videosAdd.php
@@ -1,164 +1,242 @@
 
 <div class="wrapper">
 
-  
-  <input type="submit" id="exportar"  class="exportar" value="Exportar">
-
-  <span class="uptext left">Aplicación:</span> <input type="text" name="nombreApp" id="nombreApp" class="input post left" value="<?php echo $apps['nombre']; ?>">
-  <div class="status left spinner"></div>
-
-  <div class="clear"></div>
-  
-  <nav id="menu">
-    <ul>
-      <li><a href="<?php echo base_url(); ?>apps/view/<?php echo $app; ?>" class="">Recetas</a></li>
-      <li><a href="<?php echo base_url(); ?>categorias/view/<?php echo $app; ?>" class="">Categorias</a></li>
-      <li><a href="<?php echo base_url(); ?>glosario/view/<?php echo $app; ?>" class="">Glosarios</a></li>
-      <li class="active"><a href="<?php echo base_url(); ?>videos/view/<?php echo $app; ?>" class="">Videos</a></li>
-      <li><a href="<?php echo base_url(); ?>complementarias/view/<?php echo $app; ?>" class="">Recetas complementarias</a></li>
-    </ul>
-  </nav>
-
   <div class="main">
+    <div id="status">
+     
+    </div>
+
+    <a href="<?php echo base_url() ?>" class="home">Regresar</a>
+
     <div class="columna">
-  
-      <table id="recetas" class="lista">
-        <thead>
-          <tr>
-            <td colspan="2"><input type="submit" class="button mg1 bl1" value="+ Nuevo video"></td>
-          </tr>
-          <tr>
-            <td colspan="2"><input type="text" name="" id="buscar" class="input post buscar" placeholder="Buscar.." value="">
-            <span class="postfix email">  </span></td>
-          </tr>
-        </thead>
 
-        <tbody>
-          <?php if(isset($videos))
-                {
-                  for ($i=0; $i <count($videos) ; $i++) 
-                    { 
-                      ?>
-
-                    <tr>
-                        <td class="txleft">
-                          <a class="bluetext">
-                            <?php echo $videos[$i]['video']; ?>
-                          </a>
-                        </td>
-                        <td>
-                          <a href="#eliminarGlosario<?php echo $videos[$i]['id']; ?>">Eliminar</a></td>
-                    </tr>
-                    <?php     
-                    }   
-                } ?>
-        </tbody>
-      </table>
-
-      <?php
-      if(isset($videos))
-      {
-      	
-
-        for ($i=0; $i <count($videos) ; $i++) 
-        { 
-          ?>
-          <div id="eliminarGlosario<?php echo $videos[$i]['id']; ?>" class="modalDialog">
-                      <div>
-                        <a href="#" title="Close" class="close">X</a>
-                          
-                          <?php echo form_open("videos/eliminar/") ?>
-                          <form method="post" action="<?php echo base_url(); ?>videos/eliminar/">
-        
-                            <h2><?php echo $videos[$i]['video']; ?></h2>
-                            <p>Nota: Eliminará este video de forma definitiva.</p>
-        
-                            <input type="hidden" name="id_glosario"  id="id_glosario"  value="<?php echo $videos[$i]['id']; ?>">
-                            <input type="hidden" name="app" id="id" value="<?php echo $app; ?>">
-          
-                            <button type="submit" class="eliminarBoton">Eliminar</button>
-
-                          </form>
-
-                      </div>
-                    </div>
-          <?php
-        }
-      }
-       ?>
-      
-      
+      <h1>Menu</h1>
+      <nav id="menu">
+        <ul>
+          <li><a href="<?php echo base_url().'apps/view/'.$app; ?>" class="">Recetas</a></li>
+          <li><a href="<?php echo base_url().'categorias/view/'.$app; ?>" class="">Categorias</a></li>
+          <li><a href="<?php echo base_url().'glosario/view/'.$app; ?>" class="">Glosarios</a></li>
+          <li class="active"><a href="<?php echo base_url().'videos/view/'.$app; ?>" class="">Videos</a></li>
+          <li><a href="<?php echo base_url().'complementarias/view/'.$app; ?>" id="getComplementsRecipes" class="">Recetas complementarias</a></li>
+        </ul>
+      </nav>
     </div>
     
     <div class="columna">
 
       <div id="addblock">
-        
-        <div class="myform">
-
-          <h2>Nuevo video</h2>
-        <p>Información del video</p>
-        <br>
-          
-            <?php echo validation_errors(); ?>
-            <?php echo form_open('videos/create/'.$app) ?>
-            
-            
-            
-            <label for="nombre" class="fixh1">Nombre</label>
-            <input type="text" name="nombre" id="nombre" />
-            
-            <br>
-
-            <input type="hidden" name="app" value="<?php echo $app; ?>"> 
-
-            <label for="categoria" class="fixh2">Descripcion</label>
-            <textarea type="text" name="descripcion"></textarea>
-
-            <br>
-            
-            
-            
-            <label for="proce" class="fixmargin">Imagen <span class="small">Nombre del archivo de imagen</span></label>
-            <input name="imagen" title="imagen" rows="4" cols="46">
-
-           
-            <br><br>
-            
-            <button type="submit" class="button bl2">Guardar</button>
-
-          </form>
-        </div>
-      </div>
-    
-    </div>
-  </div>
-  <div class="clear"></div>
-</div>
-<script>
- var base_url = "<?php echo base_url(); ?>";
- var app      =  "<?php echo $app; ?>";
-
-$("#nombreApp").keyup(function ()
-{
-    var nombreApp = $("#nombreApp").val();
-    console.log(nombreApp);
-
-    $.post(base_url+"apps/updateNombre/", {nombre: nombreApp, id_app: app}, function (data)
-    {
-        
-    });
-});
-
-$("#buscar").keyup(function ()
-{
   
-});
+        <div id="controles">
+          <a href="#nuevaReceta" class="button bl1">Nueva receta</a>
+          <input type="text" name="" id="buscar" class="input" placeholder="Buscar.." value="">
+        </div> 
+  
+        <table id="videos">
+          <thead>
+            <tr>
+              <td colspan="2">Recetas</td>
+            </tr>
+          </thead>
+
+          <tbody class="blockscroll">
+            <?php if(isset($videos))
+                  {
+                    for ($i=0; $i <count($videos) ; $i++) 
+                      { ?>
+
+                      <tr>
+                          <td class="txleft">
+                            <a href="<?php echo base_url().'videos/view/'.$videos[$i]['id']; ?>" class="bluetext">
+                              <?php echo $videos[$i]['video']; ?>
+                            </a>
+                          </td>
+
+                          <td>
+                            <a href="#editarGlosario<?php echo $videos[$i]['id']; ?>">
+                              Editar
+                            </a>
+                          </td>
+
+                          <td>
+                            <a href="#eliminarGlosario<?php echo $videos[$i]['id']; ?>" class='eliminarRecetas'>
+                              Eliminar
+                            </a>
+                          </td>
+
+                      </tr>
+
+                      <?php     
+                      }   
+                  } ?>
+          </tbody>
+        </table>
+
+        <?php if(isset($videos))
+                  {
+                    for ($i=0; $i <count($videos) ; $i++) 
+                      { ?>
+
+                      <div id="eliminarGlosario<?php echo $videos[$i]['id']; ?>" class="modalDialog">
+                        <div>
+                          <a href="#" title="Close" class="close">X</a>
+            
+                        <?php echo form_open("videos/delete/"); ?>
+                          <h2>Eliminar receta</h2><br><br>
+                          <div class="centrar">
+                            <label for="">Nombre: </label>
+                              <?php echo $videos[$i]['video']; ?>
+                          </div>
+                          <input type="hidden" name="id" value="<?php echo $videos[$i]['id']; ?>">
+                          <input type="hidden" name="id_app" value="<?php echo $app; ?>">
+                          <br>
+                          <button type="submit" class="eliminarBoton">Eliminar</button>
+                        </form>
+            
+                        </div>
+                      </div>
+
+                      <div id="editarGlosario<?php echo $videos[$i]['id']; ?>" class="modalDialog">
+                        <div>
+                          <a href="#" title="Close" class="close">X</a>
+            
+                        <?php echo form_open("videos/delete/"); ?>
+                          <h2>Edita los videos</h2><br><br>
+                          <div class="centrar">
+                            <label for="">Nombre: </label>
+                             <input type="text" name="video" value="<?php echo $videos[$i]['video']; ?>">
+                          </div>
+
+                          
+
+                          <input type="hidden" name="id" value="<?php echo $videos[$i]['id']; ?>">
+                          <input type="hidden" name="id_app" value="<?php echo $app; ?>">
+                          <br>
+                          <button type="submit" class="eliminarBoton">Editar</button>
+                        </form>
+            
+                        </div>
+                      </div>
+
+                      <?php } 
+                    } ?>
+        
+      </div>
+    </div>
+
+  </div>
 
 
-$("#exportar").click(function ()
-{
-  location.href=""+base_url+"export/create/"+app+"";
-});
+  <div id="nuevaReceta" class="modalDialog">
+    <div class="popup">
+      <a href="#" title="Close" class="close">x</a>
+
+      <div id="formulario">
+
+        <?php echo form_open("videos/create/"); ?>        
+        <!-- <form action=""> -->
+
+          <h2>Nueva Receta</h2>
+
+          <input type="hidden" name="id_app" value="<?php echo $app; ?>" placeholder="" required>
+
+          <div class="left">
+            <label for="">Título: </label>
+            <input type="text" name="titulo" id="titulo" value="" placeholder="Título" required>
+          </div>
+
+          <div class="left mg_input">
+            <label for="">Categoria: </label>
+            <select name="categoria">
+              <?php for ($i=0; $i <count($videos) ; $i++) { ?>
+
+                <option value="<?php echo $videos[$i]['id'] ?>">
+                  <?php echo $videos[$i]['video'] ?>
+                </option>
+
+              <?php } ?>
+            </select>
+          </div>
+
+          <div class="clear"></div>
+
+          <div class="left">
+            <label for="">Procedimiento: </label>
+            <textarea name="procedimiento" class="full"></textarea>
+          </div>
+          
+          <div class="clear"></div>
+          
+          <div class="left">
+            <label for="">Ingredientes: </label>
+            <textarea name="ingredientes" class="full"></textarea>
+          </div>
+          
+          <div class="clear"></div>
+
+          <div class="left mg_input2">
+            <label for="">Preparación: </label>
+            <input type="text" name="preparacion" value="" placeholder="minutos" required>
+          </div>
+        
+          <div class="left mg_input2">
+            <label for="">Cocción: </label>
+            <input type="text" name="coccion" value="" placeholder="minutos" required>
+          </div>
+          
+          <div class="left mg_input2">
+            <label for="">Costo: </label>
+            <select name="costo">
+              <?php for ($i=1; $i <6 ; $i++) { ?>
+                <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+              <?php } ?>
+            </select>
+          </div>
+        
+          <div class="left mg_input2">
+            <label for="">Dificultad: </label>
+            <select name="dificultad">
+              <?php for ($i=1; $i <6 ; $i++) { ?>
+                <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+              <?php } ?>
+            </select>
+          </div>
+        
+          <div class="clear"></div>
+
+          <label for="">Imagen: </label>
+          <input type="text" name="foto" value="" placeholder="" required>
+
+          <button type="submit" class="submit">Agregar</button>
+    
+        </form>
+
+      </div> <!-- formulario -->
+    </div> <!-- popup -->
+  </div> <!-- modadialog -->
+
+  <div class="clear"></div>
+
+</div> <!-- Wrapper -->
+
+<script>
+
+  var app = "<?php echo $app; ?>";
+  var base_url = "<?php echo base_url(); ?>";
+
+  $("#exportar").click(function ()
+  {
+      location.href=base_url+"export/create/"+app;
+  });
+
+
+  $("#buscar").keyup(function (data)
+  {
+    var texto = $("#buscar").val();
+
+    $.post(base_url+"recetas/searchByName/" ,{palabra: texto, id_app: app}, function (data)
+    {
+      $("#recetas tbody").html(data);
+    }); 
+  });
 
 </script>
