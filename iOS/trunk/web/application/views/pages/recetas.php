718db12ed04df41169066f5edcc26e17c7902496
j-stark <comj04@gmail.com>
2013-05-01 13:59:15 -0500

    No se, Gersain me obliga a hacer commits

--- iOS/trunk/web/application/views/pages/recetas.php
+++ iOS/trunk/web/application/views/pages/recetas.php
@@ -95,59 +95,68 @@
             <div id="editarReceta<?php echo $recetas[$i]['id']; ?>" class="modalDialog">
               <div class="popup form_receta">
                 <a href="#" title="Close" class="close">x</a>
+                  <div id="formulario">
               
-                  <?php echo form_open("recetas/delete/"); ?>
-                    <h2>Edita categoria</h2>
+                    <?php echo form_open("recetas/delete/"); ?>
 
-                    <div class="left">
-                      <label for="">Titulo: </label>
-                      <input type="text" name="nombre" value="<?php echo $recetas[$i]['titulo']; ?>">
-                    </div>
-                
-                    <div id="left mg_input">
-                      <label for="">Categoria: </label>
-                      <select name="categoria">
-                        <?php foreach ($categorias as $key => $value) { ?>
-                          <option value="<?php echo $value['id']; ?>" <?php if($recetas[$i]['id_categoria'] == $value['id']) { echo "selected"; } ?>><?php echo $value['nombre']; ?></option>
-                        <?php } ?>
-                      </select>
-                    </div>
+                      <h2 class="mgt_20">Información</h2>
 
-                    <div class="left">
-                      <label for="">Procedimiento: </label>
-                      <textarea type="text" class="full" name="procedimiento"><?php echo $recetas[$i]['procedimiento']; ?></textarea>
-                    </div>
+                      <div class="left">
+                        <label for="">Titulo: </label>
+                        <input type="text" name="nombre" value="<?php echo $recetas[$i]['titulo']; ?>">
+                      </div>
+                  
+                      <div class="left mg_input">
+                        <label for="">Categoria: </label>
+                        <select name="categoria">
+                          <?php foreach ($categorias as $key => $value) { ?>
+                            <option value="<?php echo $value['id']; ?>" <?php if($recetas[$i]['id_categoria'] == $value['id']) { echo "selected"; } ?>><?php echo $value['nombre']; ?></option>
+                          <?php } ?>
+                        </select>
+                      </div>
+                      
+                      <div class="clear"></div>
 
-                    <div class="left">
-                      <label for="">Ingredientes: </label>
-                      <textarea type="text" class="full" name="ingredientes"><?php echo $recetas[$i]['ingredientes']; ?></textarea>
-                    </div>
+                      <div class="left">
+                        <label for="">Procedimiento: </label>
+                        <textarea type="text" class="full" name="procedimiento"><?php echo $recetas[$i]['procedimiento']; ?></textarea>
+                      </div>
 
-                    <div class="left">
-                      <label for="">Preparación: </label>
-                      <input type="text" name="preparacion" value="<?php echo $recetas[$i]['preparacion']; ?>">
-                    </div>
+                      <div class="clear"></div>
 
-                    <div class="left">
-                      <label for="">Cocción: </label>
-                      <input type="text" name="coccion" value="<?php echo $recetas[$i]['coccion']; ?>">
-                    </div>
+                      <div class="left">
+                        <label for="">Ingredientes: </label>
+                        <textarea type="text" class="full" name="ingredientes"><?php echo $recetas[$i]['ingredientes']; ?></textarea>
+                      </div>
 
-                    <div class="left">
-                      <label for="">Costo: </label>
-                      <input type="text" name="costo" value="<?php echo $recetas[$i]['costo']; ?>">
-                    </div>
+                      <div class="clear"></div>
 
-                    <div class="left">
-                      <label for="">Foto: </label>
+                      <div class="left mg_input2">
+                        <label for="">Preparación: </label>
+                        <input type="text" name="preparacion" value="<?php echo $recetas[$i]['preparacion']; ?>">
+                      </div>
+
+                      <div class="left mg_input2">
+                        <label for="">Cocción: </label>
+                        <input type="text" name="coccion" value="<?php echo $recetas[$i]['coccion']; ?>">
+                      </div>
+
+                      <div class="left mg_input2">
+                        <label for="">Costo: </label>
+                        <input type="text" name="costo" value="<?php echo $recetas[$i]['costo']; ?>">
+                      </div>
+
+                      <div class="clear"></div>
+                       
+                      <label for="">Imagen: </label>
                       <input type="text" name="foto" value="<?php echo $recetas[$i]['foto']; ?>">
-                    </div>
-                    
-                    <input type="hidden" name="id" value="<?php echo $recetas[$i]['id']; ?>">
-                    <input type="hidden" name="id_app" value="<?php echo $app; ?>">
-                
-                    <button type="submit" class="submit">Guardar</button>
-                  </form>
+                      
+                      <input type="hidden" name="id" value="<?php echo $recetas[$i]['id']; ?>">
+                      <input type="hidden" name="id_app" value="<?php echo $app; ?>">
+                  
+                      <button type="submit" class="submit">Guardar</button>
+                    </form>
+                  </div>
               </div>
             </div>
           
