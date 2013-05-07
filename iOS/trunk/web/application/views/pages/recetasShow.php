4dda0e3a1164ccad98f09fad5dd0bff779987233
j-stark <comj04@gmail.com>
2013-05-07 09:39:54 -0500

    just a commit

--- iOS/trunk/web/application/views/pages/recetasShow.php
+++ iOS/trunk/web/application/views/pages/recetasShow.php
@@ -1,96 +1,92 @@
 <div class="wrapper">
 	<div class="main">
-	<ul class="slideshow">
-        <li>
-          	
-          	<!-- Dar de alta una nueva receta -->
-			 <div id="nuevaReceta" class="">
-			 	<div class="popup form_receta">
-                  <div id="formulario">
-					<h1>Dar de alta una nueva receta</h1>
+    <div id="status"></div>
 
-        				<form name="agregarRecetas" id="agregarRecetas" method="post">        
+    <a href="<?php echo base_url() ?>" class="back"><span>←</span> regresar</a>
 
-          					<h2 class="mgt_50">Nueva receta</h2>
+    <ul class="slideshow">
+      <li>
+        <div class="popup bg_grey">
 
-          					<input type="hidden" name="id_app" value="<?php echo $app; ?>" placeholder="" required>
+      			<form name="agregarRecetas" id="agregarRecetas" class="newreceta" method="post">
+              <h2 class="mgt_50">Nueva receta</h2>
 
-          					<div class="left">
-            					<label for="">Título: </label>
-            					<input type="text" name="titulo" id="titulo" value="" placeholder="Título" required>
-          					</div>
+        			<input type="hidden" name="id_app" value="<?php echo $app; ?>" placeholder="" required>
 
-          					<div class="left">
-            					<label for="">Categoria: </label>
-            					<select name="categoria">
-              					<?php	
-              						for ($i=0; $i <count($categorias) ; $i++) { ?>
-              						<option value="<?php echo $categorias[$i]['id'] ?>"><?php echo $categorias[$i]['nombre'] ?></option>
-              					<?php
-            					} ?>
-            					</select>
-          					</div>
+    					<div class="left">
+      					<label for="">Título: </label>
+      					<input type="text" name="titulo" id="titulo" value="" placeholder="Título" required>
+    					</div>
 
-          					<div class="clear"></div>
+    					<div class="left mg_input">
+      					<label for="">Categoria: </label>
+      					<select name="categoria">
+        					<?php	
+        						for ($i=0; $i <count($categorias) ; $i++) { ?>
+        						  <option value="<?php echo $categorias[$i]['id'] ?>"><?php echo $categorias[$i]['nombre'] ?></option>
+                      <?php
+      					     } ?>
+      					</select>
+    					</div>
 
-          					<div class="left">
-            					<label for="">Procedimiento: </label>
-            					<textarea name="procedimiento" class="full"></textarea>
-          					</div>
-          
-          					<div class="clear"></div>
-          					
-          					<div class="left">
-            					<label for="">Ingredientes: </label>
-            					<textarea name="ingredientes" class="full"></textarea>
-          					</div>
-          
-          					<div class="clear"></div>
+        			<div class="clear"></div>
 
-          					<div class="left mg_input2">
-            					<label for="">Preparación: </label>
-            					<input type="text" name="preparacion" value="" placeholder="minutos" required>
-          					</div>
+    					<div class="left">
+      					<label for="">Procedimiento: </label>
+      					<textarea name="procedimiento" class="full"></textarea>
+    					</div>
+    
+        			<div class="clear"></div>
+        					
+    					<div class="left">
+      					<label for="">Ingredientes: </label>
+      					<textarea name="ingredientes" class="full"></textarea>
+    					</div>
         
-          					<div class="left mg_input2">
-            					<label for="">Cocción: </label>
-            					<input type="text" name="coccion" value="" placeholder="minutos" required>
-          					</div>
-          
-          					<div class="left mg_input2">
-            					<label for="">Costo: </label>
-            					<select name="costo">
-              						<?php for ($i=1; $i <6 ; $i++) { ?>
-                						<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-              						<?php } ?>
-            					</select>
-          					</div>
-        
-          					<div class="left mg_input2">
-            					<label for="">Dificultad: </label>
-            					<select name="dificultad">
-              						<?php for ($i=1; $i <6 ; $i++) { ?>
-                						<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-              						<?php } ?>
-            					</select>
-          					</div>
-        
-          					<div class="clear"></div>
+        			<div class="clear"></div>
 
-          					<label for="">Imagen: </label>
-          					<input type="text" name="foto" value="" placeholder="" required>
+    					<div class="left mg_input2">
+      					<label for="">Preparación: </label>
+      					<input type="text" name="preparacion" value="" placeholder="minutos" required>
+    					</div>
+      
+    					<div class="left mg_input2">
+      					<label for="">Cocción: </label>
+      					<input type="text" name="coccion" value="" placeholder="minutos" required>
+    					</div>
+    
+    					<div class="left mg_input2">
+      					<label for="">Costo: </label>
+      					<select name="costo">
+        						<?php for ($i=1; $i <6 ; $i++) { ?>
+          						<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+        						<?php } ?>
+      					</select>
+    					</div>
+      
+    					<div class="left mg_input2">
+      					<label for="">Dificultad: </label>
+      					<select name="dificultad">
+        						<?php for ($i=1; $i <6 ; $i++) { ?>
+          						<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+        						<?php } ?>
+      					</select>
+    					</div>
+      
+        			<div class="clear"></div>
 
-          					<button type="submit" class="submit">Siguiente</button>
-        				</form>
-        			</div>
-        		</div>
-			</div> 	<!-- nuevaReceta -->
-			<!-- Termina dar de alta una nueva receta -->
-		</li>
+        			<label for="">Imagen: </label>
+        			<input type="text" name="foto" value="" placeholder="" required>
+
+        			<button type="submit" class="submit">Siguiente</button>
+      			</form>
+      		
+      	</div>  <!-- popup-->
+		  </li> <!-- primer elemento-->
 		  	
-        <li>
-			<div>
-		  		<input class="search" type="search" name="searchVideos" id="searchVideos" placeholder="Busqueda de videos">
+      <li>
+        <div class="bg_grey">
+          <input class="input mg_bt" type="text" name="searchVideos" id="searchVideos" placeholder="Buscar...">
 		  		
 		  		<table id="videosRelacionados">
 		  			<thead>
@@ -102,49 +98,49 @@
 		  				<tr>
 		  					<td>Video 1</td>
 		  				</tr>
-		 			</tbody>
-		  		</table>
-		  	</div>
-        </li>
+		 			  </tbody>
+		  	 </table>
+		    </div>
+      </li>
           
-        <li>
-          	<div>
-		  		<input class="search" type="search" name="searchComplementarias" id="searchComplementarias" placeholder="Busqueda de recetas complementarias">
-		  		<table id="recetasComplementarias">
-		  			<thead>
-		  				<tr>
-		  					<td>Recetas complementarias</td>
-		  				</tr>
-		  			</thead>
-		  			<tbody>
-		  				<tr>
-		  					<td>Complementaria 1</td>
-		  				</tr>
-		  			</tbody>
-		  		</table>
-		  	</div>
-        </li>
+      <li>
+        <div class="bg_grey">
+          <input class="input mg_bt" type="text" name="searchComplementarias" id="searchComplementarias" placeholder="Buscar...">
+          <table id="recetasComplementarias">
+              <thead>
+              <tr>
+                <td>Recetas complementarias</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Complementaria 1</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </li>
 
           
-        <li>
-          	<div>
-		  		<input class="search" type="search" name="searchGlosario" id="searchGlosario" placeholder="Busqueda de glorsarios para relacionar">
-		  		<table id="glosariosRelacionados">
-		  			<thead>
-		  				<tr>
-		  					<td>Glosarios relacionados</td>
-		  				</tr>
-		  			</thead>
-		  			<tbody>
-		  				<tr>
-		  					<td>Glosario 1</td>
-		  				</tr>
-		  			</tbody>
-		  		</table>
-		  	</div>
-        </li>
+      <li>
+        <div class="bg_grey">
+          <input class="input mg_bt" type="text" name="searchGlosario" id="searchGlosario" placeholder="Buscar...">
+          <table id="glosariosRelacionados">
+            <thead>
+              <tr>
+                <td>Glosarios relacionados</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Glosario 1</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </li>
      
-        </ul>
+    </ul>
 </div>
 
 <script>
