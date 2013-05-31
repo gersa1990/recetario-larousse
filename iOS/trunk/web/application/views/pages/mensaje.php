a4d171456f3628e5a802ef70eef5646059537e02
j-stark <comj04@gmail.com>
2013-05-31 17:27:23 -0500

    Mensaje

---
+++ iOS/trunk/web/application/views/pages/mensaje.php
@@ -0,0 +1,11 @@
+<div class="wrapper">
+	<div class="alert box_mensaje">
+		<div class="box">
+			<h1>No se agregaron relaciones</h1>
+
+			<a href="javascript:history.go(-1)" class="button orange">Regresar</a>
+			<a href="<?php echo base_url().'apps/view/'.$app ?>" class="button orange">Continuar</a>
+			
+		</div>
+	</div>
+</div>
